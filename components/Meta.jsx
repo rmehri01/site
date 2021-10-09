f4b31d2a5cb73cef802745a4366c2850b5714dc6
Ryan Mehri <ryan.mehri1@gmail.com>
2021-10-08 23:48:05 -0700

    Change metadata image

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { config, dom } from '@fortawesome/fontawesome-svg-core';
-import profilePic from '../public/profile.jpg';
 
 export default function Meta() {
   config.autoAddCss = false;
@@ -15,13 +14,13 @@ export default function Meta() {
       <meta property="og:url" content="https://www.ryanmehri.dev" />
       <meta property="og:title" content="Ryan Mehri :: Software Engineer" />
       <meta property="og:description" content="Hey there! I'm Ryan, a computer science student interested in programming language theory, compilers, and developer tooling." />
-      <meta property="og:image" content={profilePic} />
+      <meta property="og:image" content="/profile.jpg" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://www.ryanmehri.dev" />
       <meta property="twitter:title" content="Ryan Mehri :: Software Engineer" />
       <meta property="twitter:description" content="Hey there! I'm Ryan, a computer science student interested in programming language theory, compilers, and developer tooling." />
-      <meta property="twitter:image" content={profilePic} />
+      <meta property="twitter:image" content="/profile.jpg" />
 
       <link rel="icon" href="/favicon.ico" />
       <style>{dom.css()}</style>
