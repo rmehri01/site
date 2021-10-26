3163df0c5a916a61fa536b18632040d7985db403
Ryan Mehri <ryan.mehri1@gmail.com>
2021-10-26 12:45:25 -0700

    feat: try new next.js features and use sharp for images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp']
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
